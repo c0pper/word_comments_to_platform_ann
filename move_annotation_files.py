cc83ea62a19eda668c07d78ad5b59028ea6368d2
93simon <93simonster@gmail.com>
2022-08-04 16:03:06 +0200

    move_annotation_files

--- move_annotation_files.py
+++ move_annotation_files.py
@@ -1,20 +1,28 @@
+from itertools import count
 from pathlib import Path
 import shutil
 from datetime import datetime
 from tqdm import tqdm
 
-july_date_in_epoch = 1657497600
-date_str =datetime.fromtimestamp(july_date_in_epoch).strftime("%d-%m-%y")
-# p = Path('C:\\Users\\smarotta\\PycharmProjects\\mirco_annotazioni_server_1maggio\\Banking_-_Performing')
-p = Path('C:\\Users\\smarotta\\PycharmProjects\\word_comments_to_platform_ann\\input_docs_debug')
+
+# date_time_obj = None
+# while not date_time_obj:
+#     date_str = input("Input date in format DD-MM-YY: ")
+#     try:
+#         date_time_obj = datetime.strptime(date_str, '%d-%m-%y')
+#     except ValueError:
+#         print("Wrong format, try again.")
+    
+
+
+p = Path('C:\\Users\\smarotta\\Desktop\\trascrizioni_scaricate')
 
 # trova file con data modifica posteriore a quella fornita nelle sottocartelle del path fornito e li copia nella cartella "files form after" a livello del file script
 def get_files_from_date(path: Path, epoch: int):
     """Trova file con data modifica posteriore a quella fornita nelle sottocartelle del path fornito e li copia nella cartella "files form after" a livello del file script"""
 
     l = list(path.glob('**/*.docx'))
-    date_str =datetime.fromtimestamp(july_date_in_epoch).strftime("%d-%m-%y")
-
+    
     script_path = Path(__file__).parent
     copied_path = script_path / f'files_from_after_{date_str}'
     copied_path.mkdir(mode=0o777, exist_ok=True)
@@ -28,4 +36,21 @@ def get_files_from_date(path: Path, epoch: int):
         else:
             print(f"\n{f.name} exists already")
 
-get_files_from_date(p, july_date_in_epoch)
+
+def move_annotation_files(path: Path, destination_path: Path):
+    """ Find in the tree and move all annotated files to single destination folder"""
+    destination_path.mkdir(exist_ok=True)
+    l = list(path.glob('**/*annotato.docx'))
+    errors = 0
+    for f in tqdm(l):
+        dest_path = destination_path / f.name
+        try:
+            if not dest_path.is_file(): # file doesn't exist
+                print(f"Moving {f.name}")
+                shutil.move(f, dest_path)
+        except FileNotFoundError:
+            errors +=1
+    print("Completed with", errors, "errors")
+
+# move_annotation_files(Path('C:\\Users\\smarotta\\Desktop\\trascrizioni_scaricate'), Path('C:\\Users\\smarotta\\Desktop\\trasc_ann'))
+# get_files_from_date(p, date_time_obj.timestamp())
